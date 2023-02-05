3cb42805a7baafcb11ed25cf009a7a8bfd1cc871
Pham Van Nhat <114339708+minhnhatzc@users.noreply.github.com>
2023-02-05 11:55:53 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
+import { Link } from "react-router-dom";
+
+function Header(props) {
+  const { address, isConnected, connect } = props;
+
+  return (
+    <header>
+      <div className="header_left">
+        <img src={Logo} alt="logo" className="logo" />
+        <Link to="/swap" className="link">
+          <div className="headerItem">Swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className="headerItem">Tokens</div>
+        </Link>
+        <Link to="/chart" className="link">
+          <div className="headerItem">Chart</div>
+        </Link>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton" onClick={connect}>
+          {isConnected
+            ? address.slice(0, 5) + "..." + address.slice(38)
+            : "Connect"}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Swap.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Swap() {
+  return (
+    <div>Swap</div>
+  )
+}
+
